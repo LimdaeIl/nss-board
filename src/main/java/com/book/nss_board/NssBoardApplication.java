38f84e19bd106508a187b14c13d4333c5ec77565
LimdaeIl <optional94@naver.com>
2026-03-16 20:46:57 +0900

    init project

---
+++ src/main/java/com/book/nss_board/NssBoardApplication.java
@@ -0,0 +1,13 @@
+package com.book.nss_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NssBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NssBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/nss_board/NssBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.nss_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
